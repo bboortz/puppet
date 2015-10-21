4d5680828f9b1e6f3c5c4c451fbe5979da9b44d4
Benjamin Boortz <benjamin.boortz@volkswagen.de>
2015-10-21 18:37:02 +0200

    vw-modules /BB

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,14 @@
 node default {
 	include my-cron-puppet
 	include my-packages
+	include docker
 }
 
 node 'master' {
 }
+
+docker::image { 'base' }
+
 #docker::image { 'ubuntu':
 #	ensure => 'absent',
 #}
