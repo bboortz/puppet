b5d92e3ce0cf4aea1e4acc6137790f37fce413f3
root <root@wb-t500.fritz.box>
2015-10-23 22:21:45 +0200

    docker run removed /BB

--- my-modules/my-docker/manifests/init.pp
+++ my-modules/my-docker/manifests/init.pp
@@ -1,7 +1,9 @@
 class my-docker {
 	include docker
 
-	docker::image { 'centos': }
+	docker::image { 'centos_base': 
+		docker_file => '/appl/docker/env/images/centos_base/Dockerfile'
+	}
 
 	docker::image { 'nodejs-appl':
 		docker_file => '/appl/docker/env/images/nodejs-appl/Dockerfile'
@@ -10,21 +12,21 @@ class my-docker {
 		docker_file => '/appl/docker/env/images/nodejs-rest/Dockerfile'
 	}
 
-	docker::run { 'nodejs-appl':
-		image => 'nodejs-appl',
-		use_name => true,
-		ports => ['8080:8080'],
-		restart_service => true,
-		privileged      => false,
-		pull_on_start   => false,
-	}
+#	docker::run { 'nodejs-appl':
+#		image => 'nodejs-appl',
+#		use_name => true,
+#		ports => ['8080:8080'],
+#		restart_service => true,
+#		privileged      => false,
+#		pull_on_start   => false,
+#	}
 
-	docker::run { 'nodejs-rest':
-		image => 'nodejs-rest',
-		use_name => true,
-		ports => ['8081:8080'],
-		restart_service => true,
-		privileged      => false,
-		pull_on_start   => false,
-	}
+#	docker::run { 'nodejs-rest':
+#		image => 'nodejs-rest',
+#		use_name => true,
+#		ports => ['8081:8080'],
+#		restart_service => true,
+#		privileged      => false,
+#		pull_on_start   => false,
+#	}
 }
