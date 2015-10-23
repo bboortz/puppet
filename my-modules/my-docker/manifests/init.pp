1a953baa3d460020740e15edd266df4405bc427d
Benjamin Boortz <benjamin.boortz@volkswagen.de>
2015-10-23 05:48:22 +0200

    docker /BB

--- my-modules/my-docker/manifests/init.pp
+++ my-modules/my-docker/manifests/init.pp
@@ -11,7 +11,7 @@ class my-docker {
 	}
 
 	docker::run { 'nodejs-appl':
-		image => 'nodejs-appl:lastest',
+		image => 'nodejs-appl',
 		use_name => true,
 		ports => ['8080:8080'],
 		restart_service => true,
@@ -20,7 +20,7 @@ class my-docker {
 	}
 
 	docker::run { 'nodejs-rest':
-		image => 'nodejs-rest:lastest',
+		image => 'nodejs-rest',
 		use_name => true,
 		ports => ['8080:8081'],
 		restart_service => true,
