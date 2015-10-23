d1917e446b918a0700a5088ebe819bb980e0bf53
Benjamin Boortz <benjamin.boortz@volkswagen.de>
2015-10-23 05:27:35 +0200

    docker /BB

--- my-modules/my-docker/manifests/init.pp
+++ my-modules/my-docker/manifests/init.pp
@@ -4,13 +4,25 @@ class my-docker {
 	docker::image { 'centos': }
 
 	docker::image { 'nodejs-appl':
-		docker_file => '/appl/docker/env/images/nodejs-appl'
+		docker_file => '/appl/docker/env/images/nodejs-appl/Dockerfile'
+	}
+	docker::image { 'nodejs-rest':
+		docker_file => '/appl/docker/env/images/nodejs-rest/Dockerfile'
 	}
 
 	docker::run { 'nodejs-appl':
 		image => 'nodejs-appl',
 		use_name => true,
 		ports => ['8080:8080'],
+		restart_service => true,
+		privileged      => false,
+		pull_on_start   => false,
+	}
+
+	docker::run { 'nodejs-rest':
+		image => 'nodejs-rest',
+		use_name => true,
+		ports => ['8081:8080'],
 		restart_service => true,
 		privileged      => false,
 		pull_on_start   => false,
