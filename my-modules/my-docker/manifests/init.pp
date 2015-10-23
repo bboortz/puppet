871d4b4bce0f31f389791ea276b7d898b6119a78
Benjamin Boortz <benjamin.boortz@volkswagen.de>
2015-10-23 05:20:42 +0200

    docker /BB

--- my-modules/my-docker/manifests/init.pp
+++ my-modules/my-docker/manifests/init.pp
@@ -9,8 +9,8 @@ class my-docker {
 
 	docker::run { 'nodejs-appl':
 		image => 'nodejs-appl',
-		ports => ['8080', '8080'],
-		expose => ['8080', '8080'],
+		ports => ['8080:8080'],
+		use_names => true,
 		restart_service => true,
 		privileged      => false,
 		pull_on_start   => false,
